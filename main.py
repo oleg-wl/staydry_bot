77b0ca45ac058890c1903f906387888b4407ccb0
oleg_vl <@nitro-pc>
2023-10-08 15:44:17 +0300

    WIP Job queue

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!venv/bin/python3
 # -*- coding: UTF-8 -*-
 
-#~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~
 # Основное тело бота
-#~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~
 
 from telegram import Update
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
@@ -14,19 +14,22 @@ from telegram.ext import (
     CommandHandler,
     CallbackQueryHandler,
     MessageHandler,
-    filters
+    filters,
+    Application
 )
 
 from utils import bot_token, f, decorator_select, city_short_names
 
 import logging
+import schedule
+import time
 
 from weather import Forecast
 from db import _select, _insert, _update_city
 
 logging.basicConfig(format=f["fmt"], level=logging.DEBUG)
 
-#Логгер для http-запросов
+# Логгер для http-запросов
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
@@ -48,35 +51,32 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         _insert(user_id=update.effective_chat.id, name=update.effective_chat.username)
         await context.bot.send_message(
             chat_id=uid,
-            text=f"Приятно познакомиться {uname}. Я погодный бот.\nСкажи мне свой город и я смогу присылать тебе погоду на ближайшее время.\nОтправь мне /city и твой город\nНапример: /city Санкт-Перетбург"
+            text=f"Приятно познакомиться {uname}. Я погодный бот.\nСкажи мне свой город и я смогу присылать тебе погоду на ближайшее время.\nОтправь мне /city и твой город\nНапример: /city Санкт-Перетбург",
         )
     elif city == None:
-        
         await context.bot.send_message(
             chat_id=uid,
-            text=f"Привет {uname}. Прости я забыл твой город.\nОтправь мне /city твой город\nНапример: /city Санкт-Перетбург"
+            text=f"Привет {uname}. Прости я забыл твой город.\nОтправь мне /city твой город\nНапример: /city Санкт-Перетбург",
         )
 
     else:
         await context.bot.send_message(
             chat_id=uid,
             text=f"С возвращением {uname}, как оно? Не хочешь немного погоды в городе {city}?",
-            reply_markup=rep_marcup
+            reply_markup=rep_marcup,
         )
+
 
 async def help(update, context):
     uid = update.effective_chat.id
     uname = update.effective_chat.username
-    url = 'https://github.com/oleg-wl/staydry_bot/issues'
-    
+    url = "https://github.com/oleg-wl/staydry_bot/issues"
+
     msg = f'Привет {uname},\nБот запомнит твой город и пршлет тебе текущий прогноз и прогноз за ближайшее время\n/start - начни общение с ботом\n/city <i>_твой город_</i> поменять город\n\n&#128204; Если что-то сломалось - пиши мне <a href="{url}">на гитхабе</a>'
-    
+
     await context.bot.send_message(
-        chat_id=uid,
-        text=msg,
-        parse_mode='HTML',
-        disable_web_page_preview=True
-        )
+        chat_id=uid, text=msg, parse_mode="HTML", disable_web_page_preview=True
+    )
 
 
 async def city(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -96,11 +96,12 @@ async def city(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 #!Обернуто в декоратор из utils.py
-#Декоратор делает запрос в базу данных database.py. 
-#Возвращает сообщение с ошибкой (в чат) если в базе нет города или имени пользователя 
+# Декоратор делает запрос в базу данных database.py.
+# Возвращает сообщение с ошибкой (в чат) если в базе нет города или имени пользователя
 @decorator_select(func1=_select)
 def forecast_12h(*args, **kwargs):
-        return Forecast().weather_12h(*args, **kwargs)
+    return Forecast().weather_12h(*args, **kwargs)
+
 
 @decorator_select(func1=_select)
 def forecast_now(*args, **kwargs):
@@ -114,12 +115,13 @@ async def button(update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
 
     match q:
-        case 'forecastnow':
+        case "forecastnow":
             m = forecast_now(uid=uid)
-        case 'forecast12h':
+        case "forecast12h":
             m = forecast_12h(uid=uid)
 
     await query.edit_message_text(text=m, reply_markup=rep_marcup)
+
 
 async def reply_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
     input: str = update.message.text
@@ -129,14 +131,36 @@ async def reply_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(text=msg)
 
+
+async def scheduled_message(context: ContextTypes.DEFAULT_TYPE):
+
+    # https://docs.python-telegram-bot.org/en/v20.6/examples.timerbot.html
     
+    job = context.job
+    uid = job.chat_id
+
+    #сообщение с погодой на 12 часов
+    msg = forecast_12h(uid=uid)
+
+    await context.bot.send_message(chat_id=uid, text=msg)
+
+async def set_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
+    uid = update.effective_chat.id
+
+    #тестовый вариант секунды из contrxt.args
+    #todo: переписать на run_daily
+    sec = float(context.args[0])
+    context.job_queue.run_once(scheduled_message, sec, chat_id=uid)
+
+
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(bot_token).build()
 
     start_handler = CommandHandler("start", start)
     app.add_handler(start_handler)
-    
+
     help_handler = CommandHandler("help", help)
     app.add_handler(help_handler)
 
@@ -149,5 +173,7 @@ if __name__ == "__main__":
     msg_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, reply_msg)
     app.add_handler(msg_handler)
 
+    schedule_handler = CommandHandler("set_time", set_time)
+    app.add_handler(schedule_handler)
 
     app.run_polling()
